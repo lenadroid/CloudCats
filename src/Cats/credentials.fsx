409f1fb3586aa835c227d05f0eeebe14c4e5aa76
lenadroid <lenadroid@yandex.com>
2015-04-01 18:16:03 +0200

    cloud cats

--- src/Cats/credentials.fsx
+++ src/Cats/credentials.fsx
@@ -10,8 +10,8 @@ module ConnectionStrings =
     open MBrace.Azure.Client
     open MBrace.Azure.Runtime
 
-    let myStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cloudmbracestorage;AccountKey=U78eeobMZLKTI+YDTsYTaF+ltPvWFDGjoThO+FcXVpDkisBB1OaGlmDMZH7MjE9t3vRhpbiP/e+e60HkRh1yBw=="
-    let myServiceBusConnectionString = "Endpoint=sb://cloudmbracens.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1Hw6Ui28qK99V4FVPMMVgOwgQG4bw9IGdMd6MBho6iw="
+    let myStorageConnectionString = "your"
+    let myServiceBusConnectionString = "your"
 
     let createStorageConnectionString(storageName, storageAccessKey) = sprintf "DefaultEndpointsProtocol=https;AccountName=%s;AccountKey=%s" storageName storageAccessKey
     let createServiceBusConnectionString(serviceBusName, serviceBusKey) = sprintf "Endpoint=sb://%s.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=%s" serviceBusName serviceBusKey
